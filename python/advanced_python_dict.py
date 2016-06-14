3cd73305307fbf8a78a626a2bee8302ba8567284
Jessica Cox <Jessica@Jessicas-MacBook-Pro-7.local>
2016-06-13 22:16:47 -0400

    updating homework

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,7 @@
-PLACE YOUR CODE HERE
+import pandas as pd
+import numpy as np
+
+#import data and make into a dataframe
+faculty = pd.read_csv('/Users/Jessica/ds/metis/prework/dsp/python/faculty.csv', na_values = [""," ", 0], skipinitialspace = True)
+faculty = pd.DataFrame(faculty)
+faculty_dic_frame = pd.DataFrame(faculty)
